3d68e33bed692f5fff3e57bf795717045e88417f
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2020-04-14 14:25:38 -0700

    Adding `StoryFactory`, `StoryCreator`, `StoryBlockOperations`
    1. Previously for each method in Story factory, the functionality had been encapsulated in the constructor of the persistent class.
    2. Pieces together all the sub-entities of a Story application model and creates them in the database.
    3. An abstraction for updating and creating stories

---
+++ HourglassServer/Data/DataManipulation/StoryModel/StoryModelCreator.cs
@@ -0,0 +1,41 @@
+﻿using HourglassServer.Data.Persistent;
+using HourglassServer.Data.Application.StoryModel;
+
+/* Responsibility: Map creation actions from the StoryModel (application) to Story
+ * (persistent/database).
+ *
+ * Mapping:
+ *
+ * StoryModel       -> Story + StoryBlockModel[]
+ * StoryBlockModel  -> StoryBlock + [GeoMapBlock | TextBlock | GraphBlock]                            
+ */
+namespace HourglassServer.Data.DataManipulation.StoryModel
+{
+    public class StoryModelCreator
+    {
+        public static StoryApplicationModel AddStoryModelToDatabase(postgresContext db, StoryApplicationModel storyModel)
+        {
+            AddStoryToDatabase(db, storyModel);
+            for (int position = 0; position < storyModel.StoryBlocks.Count; position++)
+            {
+                var storyBlockModel = storyModel.StoryBlocks[position];
+                storyBlockModel.BlockPosition = position;
+                AddStoryBlockToDatabase(db, storyBlockModel, storyModel.Id);
+            }
+            return storyModel;
+        }
+
+        private static void AddStoryToDatabase(postgresContext db, StoryApplicationModel model)
+        {
+            Story newStory = StoryFactory.CreateStoryFromStoryModel(model);
+            db.Story.Add(newStory);
+        }
+
+        private static void AddStoryBlockToDatabase(postgresContext db, StoryBlockModel blockModel, string storyid)
+        {
+            StoryBlock newStoryBlock = StoryFactory.CreateStoryBlockFromStoryBlockModel(blockModel, storyid);
+            db.StoryBlock.Add(newStoryBlock);
+            TypeBlockOperations.PerformOperationOnTypeBlock(db, blockModel, TypeBlockOperations.TypeBlockOperation.ADD);
+        }
+    }
+}
---
+++ HourglassServer/Data/DataManipulation/StoryModel/TypeBlockOperations.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using HourglassServer.Data.Persistent;
+using HourglassServer.Data.Application.StoryModel;
+
+namespace HourglassServer.Data.DataManipulation.StoryModel
+{
+    public class TypeBlockOperations
+    {
+        public enum TypeBlockOperation { UPDATE, ADD };
+
+        public static void PerformOperationOnTypeBlock(postgresContext db, StoryBlockModel model, TypeBlockOperation operation)
+        {
+            StoryBlockType storyType = model.Type;
+            switch (storyType)
+            {
+                case StoryBlockType.TEXT:
+                    TextBlock textBlock = StoryFactory.CreateTextBlockFromStoryBlockModel(model);
+                    switch (operation)
+                    {
+                        case TypeBlockOperation.UPDATE:
+                            db.TextBlock.Update(textBlock);
+                            break;
+                        case TypeBlockOperation.ADD:
+                            db.TextBlock.Add(textBlock);
+                            break;
+                    }
+
+                    break;
+                case StoryBlockType.GRAPH:
+                    GraphBlock graphBlock = StoryFactory.CreateGraphBlockFromStoryBlockModel(model);
+                    switch (operation)
+                    {
+                        case TypeBlockOperation.UPDATE:
+                            db.GraphBlock.Update(graphBlock);
+                            break;
+                        case TypeBlockOperation.ADD:
+                            db.GraphBlock.Add(graphBlock);
+                            break;
+                    }
+                    break;
+                case StoryBlockType.GEOMAP:
+                    GeoMapBlock geoMapBlock = StoryFactory.CreateGeoMapBlockFromStoryBlockModel(model);
+                    db.GeoMapBlock.Update(geoMapBlock);
+                    switch (operation)
+                    {
+                        case TypeBlockOperation.UPDATE:
+                            db.GeoMapBlock.Update(geoMapBlock);
+                            break;
+                        case TypeBlockOperation.ADD:
+                            db.GeoMapBlock.Add(geoMapBlock);
+                            break;
+                    }
+                    break;
+                default:
+                    throw new ArgumentException("Could not recognize type of story block: " + storyType);
+            }
+            db.SaveChanges();
+        }
+    }
+}
