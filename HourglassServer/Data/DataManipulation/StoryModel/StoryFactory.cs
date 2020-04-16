36bafaf99ef1ad630c024217db59b331c639fbea
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2020-04-16 14:17:18 -0700

    Adding `StoryFactory` containing old constructors for persistence models

---
+++ HourglassServer/Data/DataManipulation/StoryModel/StoryFactory.cs
@@ -0,0 +1,58 @@
+﻿using HourglassServer.Data.Persistent;
+using HourglassServer.Data.Application.StoryModel;
+using System;
+
+namespace HourglassServer.Data.DataManipulation.StoryModel
+{
+    public class StoryFactory
+    {
+        public static Story CreateStoryFromStoryModel(StoryApplicationModel model)
+        {
+            Story newStory = new Story();
+            DateTime nowTimeStamp = new DateTime();
+
+            newStory.StoryId = model.Id;
+            newStory.UserId = model.UserId;
+            newStory.PublicationStatus = PublicationStatus.DRAFT.ToString();
+            newStory.Title = model.Title;
+            newStory.Description = model.Description;
+            newStory.DateCreated = nowTimeStamp;
+            newStory.DateLastEdited = nowTimeStamp;
+
+            return newStory; 
+        }
+
+        public static TextBlock CreateTextBlockFromStoryBlockModel(StoryBlockModel model)
+        {
+            TextBlock newTextBlock = new TextBlock();
+            newTextBlock.BlockId = model.Id;
+            newTextBlock.EditorState = model.EditorState;
+            return newTextBlock;
+        }
+
+        public static GraphBlock CreateGraphBlockFromStoryBlockModel(StoryBlockModel model)
+        {
+            GraphBlock newGraphBlock = new GraphBlock();
+            newGraphBlock.BlockId = model.Id;
+            newGraphBlock.GraphId = model.GraphId;
+            return newGraphBlock;
+        }
+
+        public static GeoMapBlock CreateGeoMapBlockFromStoryBlockModel(StoryBlockModel model)
+        {
+            GeoMapBlock newTextBlock = new GeoMapBlock();
+            newTextBlock.BlockId = model.Id;
+            newTextBlock.GeoMapId = model.MapId;
+            return newTextBlock;
+        }
+
+        public static StoryBlock CreateStoryBlockFromStoryBlockModel(StoryBlockModel model, string storyId)
+        {
+            StoryBlock newStoryBlock = new StoryBlock();
+            newStoryBlock.BlockId = model.Id;
+            newStoryBlock.StoryId = storyId;
+            newStoryBlock.BlockPosition = model.BlockPosition;
+            return newStoryBlock;
+        }
+    }
+}
