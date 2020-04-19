84017b79b780efed0aa906249f1b146710807e7f
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2020-04-19 14:43:51 -0700

    Adding initial code to GET and POST bookmarks

--- HourglassServer/Controllers/BookmarkController.cs
+++ HourglassServer/Controllers/BookmarkController.cs
@@ -3,7 +3,8 @@ using System.Linq;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using HourglassServer.Data;
-using HourglassServer.Data.StoryModel;
+using HourglassServer.Data.Bookmark;
+using HourglassServer.Models.Persistent;
 
 namespace HourglassServer.Controllers
 {
@@ -17,17 +18,61 @@ namespace HourglassServer.Controllers
             _context = context;
         }
 
-
         [HttpGet("{UserId}")]
-        public StoryCreationObject GetBookmarks(string UserId)
+        public List<Object> GetBookmarks(string UserId)
         {
-            throw new Exception("Method not implemented yet.");
+            List<Object> bookmarks = new List<Object>();
+
+            List<GraphBookmark> graphBookmarks = _context.GraphBookmark.Where(b => b.UserId == UserId).ToList();
+            List<StoryBookmark> storyBookmarks = _context.StoryBookmark.Where(b => b.UserId == UserId).ToList();
+            List<GeoMapBookmark> geoMapBookmarks = _context.GeoMapBookmark.Where(b => b.UserId == UserId).ToList();
+
+            bookmarks.AddRange(graphBookmarks);
+            bookmarks.AddRange(storyBookmarks);
+            bookmarks.AddRange(geoMapBookmarks);
+
+            return bookmarks;
         }
 
         [HttpPost()]
-        public StoryCreationObject CreateBookmark(BookmarkController Bookmark)
+        public IActionResult CreateBookmark(Bookmark Bookmark)
         {
-            throw new Exception("Method not implemented yet.");
+            switch(Bookmark.Type)
+            {
+                case ContentType.GRAPH:
+                    _context.GraphBookmark.Add(new GraphBookmark()
+                    {
+                        UserId = Bookmark.UserId,
+                        GraphId = Bookmark.ItemId
+                    });
+                    break;
+                case ContentType.STORY:
+                    _context.StoryBookmark.Add(new StoryBookmark()
+                    {
+                        UserId = Bookmark.UserId,
+                        StoryId = Bookmark.ItemId
+                    });
+                    break;
+                case ContentType.GEOMAP:
+                    _context.GeoMapBookmark.Add(new GeoMapBookmark()
+                    {
+                        UserId = Bookmark.UserId,
+                        GeoMapId = Bookmark.ItemId
+                    });
+                    break;
+                default:
+                    return BadRequest("Could not identify content type: " + Bookmark.Type);
+            }
+
+            try
+            {
+                _context.SaveChanges();
+            } catch(Exception e)
+            {
+                return new BadRequestObjectResult(e);
+            }
+           
+            return new OkObjectResult("success");
         }
     }
 }
--- HourglassServer/Data/Bookmark/Bookmark.cs
+++ HourglassServer/Data/Bookmark/Bookmark.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace HourglassServer.Data.Bookmark
 {
     public class Bookmark
     {
+        [Required]
         public string UserId { get; set; }
+
+        [Required]
         public string ItemId { get; set; }
+
         public ContentType Type;
 
         public Bookmark()
