3363af22d108ac71ab0ed2a254502d40d9d882bd
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2020-04-19 15:40:34 -0700

    Adding EnumConverter to `ContentType` in `BookmarkContent`

--- HourglassServer/Data/Bookmark/BookmarkContent.cs
+++ HourglassServer/Data/Bookmark/BookmarkContent.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
 namespace HourglassServer.Data.Bookmark
 {
     public class BookmarkContent
     {
+        [JsonConverter(typeof(StringEnumConverter))] //Strings not converted by default
+        public ContentType Type;
+        public object Content;
+
         public BookmarkContent()
         {
         }
