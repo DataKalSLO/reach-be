a3599fa600ab4ff1034238629c78d494dad575cb
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2020-04-19 13:48:41 -0700

    Removed methods that were only being used once

--- HourglassServer/Data/DataManipulation/StoryModel/StoryModelCreator.cs
+++ HourglassServer/Data/DataManipulation/StoryModel/StoryModelCreator.cs
@@ -1,4 +1,5 @@
-﻿using HourglassServer.Data.Persistent;
+﻿using HourglassServer.Models.Persistent;
+using HourglassServer.Data;
 using HourglassServer.Data.Application.StoryModel;
 
 /* Responsibility: Map creation actions from the StoryModel (application) to Story
@@ -13,29 +14,21 @@ namespace HourglassServer.Data.DataManipulation.StoryModel
 {
     public class StoryModelCreator
     {
-        public static StoryApplicationModel AddStoryApplicationModelToDatabaseContext(postgresContext db, StoryApplicationModel storyModel)
+        public static StoryApplicationModel AddStoryApplicationModelToDatabaseContext(HourglassContext db, StoryApplicationModel storyModel)
         {
-            AddStoryPersistenceModelToDatabaseContext(db, storyModel);
+            Story newStory = StoryFactory.CreateStoryFromStoryModel(storyModel);
+            db.Story.Add(newStory);
             for (int position = 0; position < storyModel.StoryBlocks.Count; position++)
             {
                 var storyBlockModel = storyModel.StoryBlocks[position];
                 storyBlockModel.BlockPosition = position;
-                AddStoryBlockPersistenceModelToDatabaseContext(db, storyBlockModel, storyModel.Id);
+
+                StoryBlock newStoryBlock = StoryFactory.CreateStoryBlockFromStoryBlockModel(storyBlockModel, storyModel.Id);
+                db.StoryBlock.Add(newStoryBlock);
+
+                TypeBlockOperations.PerformOperationOnTypeBlock(db, storyBlockModel, TypeBlockOperations.TypeBlockOperation.ADD);
             }
             return storyModel;
-        }
-
-        private static void AddStoryPersistenceModelToDatabaseContext(postgresContext db, StoryApplicationModel model)
-        {
-            Story newStory = StoryFactory.CreateStoryFromStoryModel(model);
-            db.Story.Add(newStory);
-        }
-
-        private static void AddStoryBlockPersistenceModelToDatabaseContext(postgresContext db, StoryBlockModel blockModel, string storyid)
-        {
-            StoryBlock newStoryBlock = StoryFactory.CreateStoryBlockFromStoryBlockModel(blockModel, storyid);
-            db.StoryBlock.Add(newStoryBlock);
-            TypeBlockOperations.PerformOperationOnTypeBlock(db, blockModel, TypeBlockOperations.TypeBlockOperation.ADD);
         }
     }
 }
