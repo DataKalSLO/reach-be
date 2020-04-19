0bb0e3d1396d0038b8de800e468b38f6bd3b9365
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2020-04-19 13:47:54 -0700

    Added generalized method for updating, creating, and deleting StoryBlocks

--- HourglassServer/Data/DataManipulation/StoryModel/TypeBlockOperations.cs
+++ HourglassServer/Data/DataManipulation/StoryModel/TypeBlockOperations.cs
@@ -1,58 +1,52 @@
 ﻿using System;
-using HourglassServer.Data.Persistent;
+using HourglassServer.Models.Persistent;
+using HourglassServer.Data;
 using HourglassServer.Data.Application.StoryModel;
+using Microsoft.EntityFrameworkCore;
 
 namespace HourglassServer.Data.DataManipulation.StoryModel
 {
     public class TypeBlockOperations
     {
-        public enum TypeBlockOperation { UPDATE, ADD };
+        public enum TypeBlockOperation { UPDATE, ADD, DELETE };
 
-        public static void PerformOperationOnTypeBlock(postgresContext db, StoryBlockModel model, TypeBlockOperation operation)
+        public static void PerformOperationOnTypeBlock(HourglassContext db, StoryBlockModel model, TypeBlockOperation operation)
         {
             StoryBlockType storyType = model.Type;
             switch (storyType)
             {
                 case StoryBlockType.TEXTDB:
                     TextBlock textBlock = StoryFactory.CreateTextBlockFromStoryBlockModel(model);
-                    switch (operation)
-                    {
-                        case TypeBlockOperation.UPDATE:
-                            db.TextBlock.Update(textBlock);
-                            break;
-                        case TypeBlockOperation.ADD:
-                            db.TextBlock.Add(textBlock);
-                            break;
-                    }
-
+                    PerformOperationOnDbSet<TextBlock>(db.TextBlock, operation, textBlock);
                     break;
                 case StoryBlockType.GRAPH:
                     GraphBlock graphBlock = StoryFactory.CreateGraphBlockFromStoryBlockModel(model);
-                    switch (operation)
-                    {
-                        case TypeBlockOperation.UPDATE:
-                            db.GraphBlock.Update(graphBlock);
-                            break;
-                        case TypeBlockOperation.ADD:
-                            db.GraphBlock.Add(graphBlock);
-                            break;
-                    }
+                    PerformOperationOnDbSet<GraphBlock>(db.GraphBlock, operation, graphBlock);
                     break;
                 case StoryBlockType.GEOMAP:
                     GeoMapBlock geoMapBlock = StoryFactory.CreateGeoMapBlockFromStoryBlockModel(model);
-                    db.GeoMapBlock.Update(geoMapBlock);
-                    switch (operation)
-                    {
-                        case TypeBlockOperation.UPDATE:
-                            db.GeoMapBlock.Update(geoMapBlock);
-                            break;
-                        case TypeBlockOperation.ADD:
-                            db.GeoMapBlock.Add(geoMapBlock);
-                            break;
-                    }
+                    PerformOperationOnDbSet<GeoMapBlock>(db.GeoMapBlock, operation, geoMapBlock);
                     break;
                 default:
                     throw new ArgumentException("Could not recognize type of story block: " + storyType);
+            }
+        }
+
+        public static void PerformOperationOnDbSet<T>(DbSet<T> dbSet, TypeBlockOperation operation, T model) where T : class
+        {
+            switch (operation)
+            {
+                case TypeBlockOperation.UPDATE:
+                    dbSet.Update(model);
+                    break;
+                case TypeBlockOperation.ADD:
+                    dbSet.Add(model);
+                    break;
+                case TypeBlockOperation.DELETE:
+                    dbSet.Remove(model);
+                    break;
+                default:
+                    throw new ArgumentException("Could not recognize operation: " + operation.ToString());
             }
         }
     }
