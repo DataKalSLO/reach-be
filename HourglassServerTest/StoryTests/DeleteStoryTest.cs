540e0978cd2f76ee9868a5dbb880c1dabaffcc43
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2020-04-25 12:01:34 -0700

    Adding unit test for `StoryModelDeleter`

---
+++ HourglassServerTest/StoryTests/DeleteStoryTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using HourglassServer.Data;
+using HourglassServer.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HourglassServerTest.StoryTests
+{
+    [TestClass]
+    public class DeleteStoryTest
+    {
+        [TestMethod]
+        public void TestDeleteStoryByIdFromController()
+        {
+            StoryTestData testData = new StoryTestData();
+            HourglassContext mockContext = testData.GetMockContext();
+            StoryController storyController = new StoryController(mockContext);
+            storyController.DeleteStoryById(testData.StoryId);
+            GeneralAssertions.AssertDbSetHasCount(mockContext.Story, 0);
+        }
+    }
+}
