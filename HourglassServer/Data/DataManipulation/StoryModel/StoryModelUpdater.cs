e9fae38b3b1f6d662901840486d31aa0e128dcc5
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2020-04-25 11:33:52 -0700

    Adding `StoryModelUpdater`
    This function handles updating all the sub-entities associated with a Story.

---
+++ HourglassServer/Data/DataManipulation/StoryModel/StoryModelUpdater.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using HourglassServer.Models.Persistent;
+using HourglassServer.Data.Application.StoryModel;
+
+// Responsibility: Update any sub-part of a story
+namespace HourglassServer.Data.DataManipulation.StoryModel
+{
+    public static class StoryModelUpdater
+    {
+        public static StoryApplicationModel UpdateStoryApplicationModel(HourglassContext db, StoryApplicationModel storyModel)
+        {
+            Story storyWithId = StoryFactory.CreateStoryFromStoryModel(storyModel); //Isolates the Story part
+            db.Story.Update(storyWithId);
+            List<string> storyBlockIds = new List<string>();
+            for(int position=0;position<storyModel.StoryBlocks.Count; position++)
+            {
+                var storyBlockModel = storyModel.StoryBlocks[position];
+                storyBlockIds.Add(storyBlockModel.Id);
+                TypeBlockOperations.PerformOperationOnTypeBlock(db, storyBlockModel, TypeBlockOperations.TypeBlockOperation.UPDATE, storyModel.Id);
+            }
+            //TODO: Query storyblocks and gather BlockIds missing in given storyModel
+            //TODO: Call delete on StoryBlockIds above
+            return storyModel;
+        }
+    }
+}
