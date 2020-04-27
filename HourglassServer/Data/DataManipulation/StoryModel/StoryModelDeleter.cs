2cf85fa125b369748cd49069466e49720920926c
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2020-04-27 12:05:40 -0700

    Fixed bug where persistent story was not being deleted.

--- HourglassServer/Data/DataManipulation/StoryModel/StoryModelDeleter.cs
+++ HourglassServer/Data/DataManipulation/StoryModel/StoryModelDeleter.cs
@@ -7,16 +7,11 @@ namespace HourglassServer.Data.DataManipulation.StoryModel
 {
     public static class StoryModelDeleter
     {
-        /* Note, I was unable to implement Foreign Key constraints on my persistent models
-         * leading to no native cascading delete operation. However, postgres does have this
-         * compability which I utilize below to delete all related entities after deleting a Story.
-         */
         public static string DeleteStoryByID(HourglassContext db, string storyId)
         {
             StoryApplicationModel storyApplicationModelToDelete = StoryModelRetriever.GetStoryApplicationModelById(db, storyId);
-            db.Remove(storyToDelete);
-
             Story storyToDelete = StoryFactory.ExtractPersistentStoryFromApplicationStory(storyApplicationModelToDelete);
+            db.Story.Remove(storyToDelete);
             foreach (StoryBlockModel storyBlockModel in storyApplicationModelToDelete.StoryBlocks)
                 TypeBlockOperations.PerformOperationOnTypeBlock(db, storyBlockModel, TypeBlockOperations.TypeBlockOperation.DELETE, storyId);
             return storyId;
