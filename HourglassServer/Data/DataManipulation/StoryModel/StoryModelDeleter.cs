70bfd696d4d2f482e18cc00d85844e6ecaaeb731
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2020-04-27 12:05:40 -0700

    Fixed bug where persistent story was not being deleted.
    # Conflicts:
    #	HourglassServer/Data/DataManipulation/StoryModel/StoryModelDeleter.cs

---
+++ HourglassServer/Data/DataManipulation/StoryModel/StoryModelDeleter.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using HourglassServer.Models.Persistent;
+using HourglassServer.Data.Application.StoryModel;
+
+// Responsibility: Delete all entities associated with a Story
+namespace HourglassServer.Data.DataManipulation.StoryModel
+{
+    public static class StoryModelDeleter
+    {
+        public static string DeleteStoryByID(HourglassContext db, string storyId)
+        {
+            StoryApplicationModel storyApplicationModelToDelete = StoryModelRetriever.GetStoryApplicationModelById(db, storyId);
+            Story storyToDelete = StoryFactory.ExtractPersistentStoryFromApplicationStory(storyApplicationModelToDelete);
+            db.Story.Remove(storyToDelete);
+            foreach (StoryBlockModel storyBlockModel in storyApplicationModelToDelete.StoryBlocks)
+                TypeBlockOperations.PerformOperationOnTypeBlock(db, storyBlockModel, TypeBlockOperations.TypeBlockOperation.DELETE, storyId);
+            return storyId;
+        }
+    }
+}
