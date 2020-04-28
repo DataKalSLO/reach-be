53e588bda09ec61f430dc4801df580048a981053
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2020-04-27 22:41:25 -0700

    PR Review Changes
    - Moved `PerformOperationOnDbSet` to its own file + renamed `TypeBlockOperation` -> `MutatorOperations`
    - Changed string in test to be valid JSON

---
+++ HourglassServer/Data/DataManipulation/DbSetOperations/Mutator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace HourglassServer.Data.DataManipulation.DbSetOperations
+{
+    public enum MutatorOperations { UPDATE, ADD, DELETE };
+
+    public class DbSetMutator
+    {
+        public static void PerformOperationOnDbSet<T>(DbSet<T> dbSet, MutatorOperations operation, T model) where T : class
+        {
+            switch (operation)
+            {
+                case MutatorOperations.UPDATE:
+                    dbSet.Update(model);
+                    break;
+                case MutatorOperations.ADD:
+                    dbSet.Add(model);
+                    break;
+                case MutatorOperations.DELETE:
+                    dbSet.Remove(model);
+                    break;
+                default:
+                    throw new ArgumentException("Could not recognize operation: " + operation.ToString());
+            }
+        }
+    }
+}
