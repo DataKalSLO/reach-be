c04f8fa508b78446074254408436981f12fd8718
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2020-05-03 13:57:33 -0700

    Updating unit tests to check for adding new story blocks
    Note, could not test deleting story blocks because this requires mocking two more methods `Find` and `Any`. This will require substantial code and will be handled in a future PR were I refactor my testing data.

--- HourglassServerTest/StoryTests/UpdateStoryTest.cs
+++ HourglassServerTest/StoryTests/UpdateStoryTest.cs
@@ -19,9 +19,49 @@ namespace HourglassServerTest.StoryTests
             StoryTestData testData = new StoryTestData();
             string newTitle = "This is a new title";
             string newEditorState = "HelloThisIsAnEditorState";
+            StoryBlockModel updatedTextBlock = new StoryBlockModel()
+            {
+                Id = testData.TextBlockId,
+                Type = StoryBlockType.TEXTDB,
+                EditorState = newEditorState
+            };
+
+            List<StoryBlockModel> storyBlocks = new List<StoryBlockModel> { updatedTextBlock };
+            StoryApplicationModel story = new StoryApplicationModel()
+            {
+                Id = testData.StoryId,
+                Title = newTitle,
+                StoryBlocks = storyBlocks
+            };
+            HourglassContext mockContext = testData.GetMockContext();
+            
+            StoryController storyController = new StoryController(mockContext);
+            storyController.ModifyStory(story);
+
+            List<Story> stories = mockContext.Story.ToList();
+            GeneralAssertions.AssertListHasCount(stories, 1);
+            Story testStory = stories[0];
+            Assert.AreEqual(testData.StoryId, testStory.StoryId);
+
+            testData.StoryDbSet.Verify(mock => mock.Update(It.IsAny<Story>()), Times.AtLeastOnce());
+            testData.TextBlockDbSet.Verify(mock => mock.Update(It.IsAny<TextBlock>()), Times.AtLeastOnce());
+            testData.TextBlockDbSet.Verify(mock => mock.Add(It.IsAny<TextBlock>()), Times.Never()); // Nothing new being added
+            testData.StoryBlockDbSet.Verify(mock => mock.Update(It.IsAny<StoryBlock>()), Times.AtLeastOnce());
+        }
+
+        [TestMethod]
+        public void UpdateStoryFromControllerContainingNewTextBlock()
+        {
+            StoryTestData testData = new StoryTestData();
+            HourglassContext mockContext = testData.GetMockContext();
+            StoryController storyController = new StoryController(mockContext);
+
+            string newTitle = "This is a new title";
+            string newEditorState = "HelloThisIsAnEditorState";
+
             StoryBlockModel newTextBlock = new StoryBlockModel()
             {
-                Id = System.Guid.NewGuid().ToString(), //TODO: Replace with call to test data after refactor PR
+                Id = System.Guid.NewGuid().ToString(),
                 Type = StoryBlockType.TEXTDB,
                 EditorState = newEditorState
             };
@@ -33,19 +73,14 @@ namespace HourglassServerTest.StoryTests
                 Title = newTitle,
                 StoryBlocks = storyBlocks
             };
-            HourglassContext mockContext = testData.GetMockContext();
             
-            StoryController storyController = new StoryController(mockContext);
-            storyController.UpdateStory(story);
+            storyController.ModifyStory(story);
 
-            List<Story> stories = mockContext.Story.ToList();
-            GeneralAssertions.AssertListHasCount(stories, 1);
-            Story testStory = stories[0];
-            Assert.AreEqual(testData.StoryId, testStory.StoryId);
-
-            testData.StoryDbSet.Verify(mock => mock.Update(It.IsAny<Story>()), Times.Once());
-            testData.TextBlockDbSet.Verify(mock => mock.Update(It.IsAny<TextBlock>()), Times.Once());
-            testData.StoryBlockDbSet.Verify(mock => mock.Update(It.IsAny<StoryBlock>()), Times.Once());
+            testData.StoryDbSet.Verify(mock => mock.Update(It.IsAny<Story>()), Times.AtLeastOnce());
+            testData.TextBlockDbSet.Verify(mock => mock.Update(It.IsAny<TextBlock>()), Times.Never());
+            testData.TextBlockDbSet.Verify(mock => mock.Add(It.IsAny<TextBlock>()), Times.AtLeastOnce()); // For new TextBlock
         }
+
+        //TODO: When `Find` and `Any` are mocked test that story blocks get deleted. Testing in postman for time being.
     }
 }
