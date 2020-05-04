9471c70bb0868e1fbbebc85e2a45867837a54e54
Ahmed Shalan <46012410+ahmedshalan@users.noreply.github.com>
2020-05-03 17:03:09 -0700

    Apache server HTTPS setup and redirect (#42)

--- HourglassServer/Startup.cs
+++ HourglassServer/Startup.cs
@@ -74,6 +74,7 @@ namespace HourglassServer
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
+            app.UseHttpsRedirection();
             app.UseCors(MyAllowSpecificOrigins);
             app.UseRouting();
             app.UseAuthentication();
