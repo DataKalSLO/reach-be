4710b1e644a2626c66ffc884bdb98d2546e63c86
Ahmed Shalan <46012410+ahmedshalan@users.noreply.github.com>
2020-05-05 09:21:45 -0700

    Removing https redirection middleware (#44)

--- HourglassServer/Startup.cs
+++ HourglassServer/Startup.cs
@@ -74,7 +74,6 @@ namespace HourglassServer
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
-            app.UseHttpsRedirection();
             app.UseCors(MyAllowSpecificOrigins);
             app.UseRouting();
             app.UseAuthentication();
