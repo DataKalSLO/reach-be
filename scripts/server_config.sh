33fbb0d085ca977897a8b6867d83562406b04dd5
Ahmed Shalan <46012410+ahmedshalan@users.noreply.github.com>
2020-05-07 10:57:29 -0700

    HTTPS production hotfix (#53)
    * Apache server HTTPS setup and redirect (#42)
    
    * Removing https redirection middleware (#44)

--- scripts/server_config.sh
+++ scripts/server_config.sh
@@ -1,5 +1,14 @@
 # Copy over Apache server configuration
-sudo cp /home/ec2-user/Hourglass/scripts/server.conf /etc/httpd/conf.d/
+# Different server configuration files are used for the staging server and the production server
+if [ "$APPLICATION_NAME" == "hourglass-server-staging" ]
+then
+    sudo cp /home/ec2-user/Hourglass/scripts/server_staging.conf /etc/httpd/conf.d/server.conf
+fi
+
+if [ "$APPLICATION_NAME" == "hourglass-server-production" ]
+then
+    sudo cp /home/ec2-user/Hourglass/scripts/server_production.conf /etc/httpd/conf.d/server.conf
+fi
 
 # Start the Apache service
 sudo systemctl start httpd.service
