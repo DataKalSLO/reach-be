6d0f34b64445d94593960879283a13162cbe8060
Ahmed Shalan <shalanahmedj@gmail.com>
2020-05-07 03:03:05 -0700

    Updated database tables to support graph feature and ran script

---
+++ HourglassServer/Models/Persistent/GraphSources.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HourglassServer.Models.Persistent
+{
+    public partial class GraphSources
+    {
+        public string GraphId { get; set; }
+        public string SeriesType { get; set; }
+        public string DatasetName { get; set; }
+        public string ColumnNames { get; set; }
+
+        public virtual DatasetMetaData DatasetNameNavigation { get; set; }
+        public virtual Graph Graph { get; set; }
+    }
+}
