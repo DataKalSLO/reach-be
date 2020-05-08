01e1cd6c09eede1e4527fcb9bec08bb0e2525d48
Ahmed Shalan <shalanahmedj@gmail.com>
2020-05-08 16:58:24 -0700

    Removing comment

--- HourglassServer/Startup.cs
+++ HourglassServer/Startup.cs
@@ -69,8 +69,6 @@ namespace HourglassServer
             services.AddMemoryCache();
 
             services.AddControllers().AddNewtonsoftJson();
-
-           // services.AddControllers().AddNewtonsoftJson(x => x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
