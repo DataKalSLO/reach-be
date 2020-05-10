c36bf24f0285bfaff7eabe2281622cfbd6957138
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2020-05-10 03:08:09 -0700

    Removed redundant query to fix tracking error

--- HourglassServer/Data/DataManipulation/StoryModel/StoryModelDeleter.cs
+++ HourglassServer/Data/DataManipulation/StoryModel/StoryModelDeleter.cs
@@ -10,9 +10,10 @@ namespace HourglassServer.Data.DataManipulation.StoryModel
     {
         public static void DeleteStoryById(HourglassContext db, string storyId)
         {
-            List<StoryBlockModel> storyBlocks = StoryModelRetriever.GetStoryBlocksByStoryId(db, storyId);            
-            foreach (StoryBlockModel storyBlockModel in storyBlocks)
-                TypeBlockOperations.MutateTypeBlock(db, storyBlockModel, DbSetOperations.MutatorOperations.DELETE, storyId);
+            List<StoryBlockModel> storyBlocks = StoryModelRetriever.GetStoryBlocksByStoryId(db, storyId);
+            db.GeoMapBlock.RemoveRange(db.GeoMapBlock.Where(geoMapBlock => geoMapBlock.StoryId == storyId));
+            db.GraphBlock.RemoveRange(db.GraphBlock.Where(graphBlock => graphBlock.StoryId == storyId));
+            db.TextBlock.RemoveRange(db.TextBlock.Where(textBlock => textBlock.StoryId == storyId));
             db.Story.Remove(db.Story.Find(storyId));
         }
     }
