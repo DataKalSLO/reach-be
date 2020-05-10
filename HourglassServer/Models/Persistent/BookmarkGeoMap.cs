5172936c7030233c5785e379d9943486c772ba7d
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2020-05-09 18:55:32 -0700

    Adding models for bookmarking stories, geo maps, and graphs

---
+++ HourglassServer/Models/Persistent/BookmarkGeoMap.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HourglassServer.Models.Persistent
+{
+    public partial class BookmarkGeoMap
+    {
+        public string UserId { get; set; }
+        public string GeoMapId { get; set; }
+
+        public virtual GeoMap GeoMap { get; set; }
+    }
+}
