efb370c4603cfa0ddb3b725b1c1080469a724e6e
Ahmed Shalan <shalanahmedj@gmail.com>
2020-05-12 03:01:48 -0700

    Fixed merge conflicts

--- HourglassServer/Models/Persistent/Graph.cs
+++ HourglassServer/Models/Persistent/Graph.cs
@@ -19,11 +19,8 @@ namespace HourglassServer.Models.Persistent
         public string SnapshotUrl { get; set; }
         public string GraphOptions { get; set; }
 
-<<<<<<< HEAD
         public virtual Person User { get; set; }
-=======
         public virtual ICollection<BookmarkGraph> BookmarkGraph { get; set; }
->>>>>>> 592a3b74590708f76b3160a7e34fed1414fd77bb
         public virtual ICollection<GraphBlock> GraphBlock { get; set; }
         public virtual ICollection<GraphSource> GraphSource { get; set; }
     }
