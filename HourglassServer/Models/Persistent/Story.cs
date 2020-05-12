606dae151c795c7cbab544a1b78bdec62a8d0fba
Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
2020-05-11 17:21:51 -0700

    Fixing Persistent model after merge (conflict required the change)

--- HourglassServer/Models/Persistent/Story.cs
+++ HourglassServer/Models/Persistent/Story.cs
@@ -7,7 +7,8 @@ namespace HourglassServer.Models.Persistent
     {
         public Story()
         {
-            BookmarkStory = new HashSet<BookmarkStory>();
+            BookmarkStory = new HashSet<BookmarkStory>();            
+            GeoMapBlock = new HashSet<GeoMapBlock>();
             GraphBlock = new HashSet<GraphBlock>();
             StoryCategory = new HashSet<StoryCategory>();
             TextBlock = new HashSet<TextBlock>();
@@ -22,6 +23,7 @@ namespace HourglassServer.Models.Persistent
         public DateTime DateLastEdited { get; set; }
 
         public virtual Person User { get; set; }
+        public virtual ICollection<GeoMapBlock> GeoMapBlock { get; set; }
         public virtual ICollection<GraphBlock> GraphBlock { get; set; }
         public virtual ICollection<BookmarkStory> BookmarkStory { get; set; }
         public virtual ICollection<StoryCategory> StoryCategory { get; set; }
