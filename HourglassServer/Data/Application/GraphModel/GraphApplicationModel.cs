1fc324212fc7951b40caa56dee5499b761fc17e5
Ahmed Shalan <shalanahmedj@gmail.com>
2020-05-13 06:20:53 -0700

    Adding custom json coverter for GraphSource

--- HourglassServer/Data/Application/GraphModel/GraphApplicationModel.cs
+++ HourglassServer/Data/Application/GraphModel/GraphApplicationModel.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using HourglassServer.Models.Persistent;
 
 namespace HourglassServer.Data.Application.GraphModel
@@ -16,4 +20,44 @@ namespace HourglassServer.Data.Application.GraphModel
 
         public string GraphOptions { get; set; }
     }
+
+    // Custom Serializer for GraphSource for it to exclude nested attributes
+    // and only serialize the string attributes
+    public class GraphSourceConverter : JsonConverter<GraphSource>
+    {
+        public override void WriteJson(JsonWriter writer, GraphSource value, JsonSerializer serializer)
+        {
+            Console.WriteLine("Im here!");
+            if (value == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+
+            var properties = value.GetType().GetProperties()
+                .Where(p => p.PropertyType == typeof(string) || p.PropertyType == typeof(string[]));
+
+            writer.WriteStartObject();
+
+            foreach (var property in properties)
+            {
+                writer.WritePropertyName(property.Name);
+                serializer.Serialize(writer, property.GetValue(value, null));
+            }
+
+            writer.WriteEndObject();
+        }
+
+        public override GraphSource ReadJson(
+                JsonReader reader,
+                Type objectType,
+                GraphSource existingValue,
+                bool hasExistingValue,
+                JsonSerializer serializer)
+        {
+            // Only support serialization with this converter
+            throw new NotImplementedException();
+        }
+    }
+
 }
