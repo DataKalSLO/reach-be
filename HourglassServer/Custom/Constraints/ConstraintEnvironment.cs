dcf8394787e72dc76515e17f9093eec73dabd85b
Alberto Rodriguez <arodr243@calpoly.edu>
2020-05-23 13:27:30 -0700

    user authentication for story api (#70)
    * Creating Utility function for getting user id from token
    
    - Added
    
    * Reduced scope of parameter to avoid unnecessary dependancies
    
    * Fix HasRole function to currently check user roles
    
    * Modified GetUserId to be an extension method
    
    * Added Permission Checking for updating stories
    
    * Refactored Permissions to be extendable + easy to reuse
    
    * Refactoring error responses
    
    * Adding `PublicationStatus` + Removing string enum converter from `StoryApplicationModel`
    
    * Refactoring permissions in story controller
    
    * Updating tests to not use controllers now that permissions are implemented
    
    * replaced magic string with constant
    
    * Removing unused imports
    
    * Adding file containing the error tags for exceptions sent to the FEND
    
    * Clarified naming of permission (Constraint) + removed redundant tag
    
    * PR changes: renaming permissions, missed ErrorTag uses, and updating ambiguous comments
    
    * Refactored `StoryContraintChecker`
    
    Co-authored-by: Alberto Rodriguez <vhsalbertorodriguez@gmail.com>
    Co-authored-by: Ahmed Shalan <shalanahmedj@gmail.com>

---
+++ HourglassServer/Custom/Constraints/ConstraintEnvironment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Claims;
+using HourglassServer.Data;
+
+namespace HourglassServer.Custom.Constraints
+{
+    public class ConstraintEnvironment
+    {
+        public ConstraintEnvironment(ClaimsPrincipal user, HourglassContext context)
+        {
+            this.user = user;
+            this.context = context;
+        }
+
+        public ClaimsPrincipal user { get; set; }
+        public HourglassContext context { get; set; }
+    }
+}
