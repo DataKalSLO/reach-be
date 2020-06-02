2e3fb44965f805d79e622eaf6513f2d8e0f2d28f
leviwthompson <leviwthompson@gmail.com>
2020-06-02 16:49:23 -0700

    Email templating
    * Add email templates, move sending to EmailService
    * Edit smtp sending config

---
+++ HourglassServer/Mail/HtmlFormatters.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HourglassServer.Mail
+{
+    public static class HtmlFormatters
+    {
+        public static string BuildBodyFromTemplate(string messageBody)
+        {
+            using StreamReader reader = File.OpenText("Mail\\email_template.html");
+            string body = reader.ReadToEnd();
+            return body.Replace("{{MESSAGE_BODY}}", messageBody); ;
+        }
+        public static string GenerateLink(string url, string text)
+        {
+            return $"<a href=\"{url}\">{text}</a>";
+        }
+    }
+}
