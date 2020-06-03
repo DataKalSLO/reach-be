39e376d8b5a10c4831291cffd6cc922c0bb2b7aa
Zach Mitchell <zdmitche@calpoly.edu>
2020-06-02 17:45:47 -0700

    Email template: make file cross platform
    Because we deploy on Linux we can't use "\" here.
    `Path.DirectorySeparatorChar` uses the correct directory separator your
    OS environment needs.

--- HourglassServer/Mail/HtmlFormatters.cs
+++ HourglassServer/Mail/HtmlFormatters.cs
@@ -8,9 +8,11 @@ namespace HourglassServer.Mail
 {
     public static class HtmlFormatters
     {
+        private const string emailTemplate = "email_template.html";
+
         public static string BuildBodyFromTemplate(string messageBody)
         {
-            using StreamReader reader = File.OpenText("Mail\\email_template.html");
+            using StreamReader reader = File.OpenText($"Mail{Path.DirectorySeparatorChar}{emailTemplate}");
             string body = reader.ReadToEnd();
             return body.Replace("{{MESSAGE_BODY}}", messageBody); ;
         }
