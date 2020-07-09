6297a5ccc0ac756c0a2c26c19baa67c78211482c
Daniel Heyde <Daniel-Heyde@users.noreply.github.com>
2020-07-09 13:11:27 -0700

    Update with new pipeline names

--- scripts/server_config.sh
+++ scripts/server_config.sh
@@ -1,11 +1,13 @@
 # Copy over Apache server configuration
-# Different server configuration files are used for the staging server and the production server
-if [ "$APPLICATION_NAME" == "hourglass-server-staging" ]
+# Different server configuration files are used for the development server and the production server
+# backend development/staging pipeline
+if [ "$APPLICATION_NAME" == "reach-be-dev" ]
 then
     sudo cp /home/ec2-user/Hourglass/scripts/server_staging.conf /etc/httpd/conf.d/server.conf
 fi
 
-if [ "$APPLICATION_NAME" == "hourglass-server-production" ]
+# backend production/release pipeline
+if [ "$APPLICATION_NAME" == "reach-be-prod" ]
 then
     sudo cp /home/ec2-user/Hourglass/scripts/server_production.conf /etc/httpd/conf.d/server.conf
 fi
