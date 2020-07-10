f14accc37dffe69a444b22b157a5c29c720d7f9c
Daniel Heyde <Daniel-Heyde@users.noreply.github.com>
2020-07-10 11:10:46 -0700

    Update server_config.sh

--- scripts/server_config.sh
+++ scripts/server_config.sh
@@ -1,11 +1,11 @@
 # Copy over Apache server configuration
-# Different server configuration files are used for the staging server and the production server
-if [ "$APPLICATION_NAME" == "hourglass-server-staging" ]
+# Different server configuration files are used for the dev/staging server and the production/release server
+if [ "$APPLICATION_NAME" == "reach-be-dev" ]
 then
     sudo cp /home/ec2-user/Hourglass/scripts/server_staging.conf /etc/httpd/conf.d/server.conf
 fi
 
-if [ "$APPLICATION_NAME" == "hourglass-server-production" ]
+if [ "$APPLICATION_NAME" == "reach-be-prod" ]
 then
     sudo cp /home/ec2-user/Hourglass/scripts/server_production.conf /etc/httpd/conf.d/server.conf
 fi
