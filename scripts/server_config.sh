50da31bb8561b6092d6d66e332619242a9440dc8
Daniel Heyde <Daniel-Heyde@users.noreply.github.com>
2020-07-13 13:07:05 -0700

    Update server_config.sh

--- scripts/server_config.sh
+++ scripts/server_config.sh
@@ -1,11 +1,12 @@
 # Copy over Apache server configuration
 # Different server configuration files are used for the dev/staging server and the production/release server
-if [ "$APPLICATION_NAME" == "reach-be-dev" ]
+# these are the application names specified in CodeDeploy
+if [ "$APPLICATION_NAME" == "reach-be-deploy-dev" ]
 then
     sudo cp /home/ec2-user/Hourglass/scripts/server_staging.conf /etc/httpd/conf.d/server.conf
 fi
 
-if [ "$APPLICATION_NAME" == "reach-be-prod" ]
+if [ "$APPLICATION_NAME" == "reach-be-deploy-prod" ]
 then
     sudo cp /home/ec2-user/Hourglass/scripts/server_production.conf /etc/httpd/conf.d/server.conf
 fi
