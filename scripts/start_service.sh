80ebd244617e3dd1816b2b20c85a36f7395b04a9
Daniel Heyde <Daniel-Heyde@users.noreply.github.com>
2020-07-16 14:07:52 -0700

    Revert to old server start command

--- scripts/start_service.sh
+++ scripts/start_service.sh
@@ -10,17 +10,4 @@ sudo chmod 755 $LOG_FILE
 
 # Start the ASP.NET core application
 cd $SERVICE_ROOT/build_output
-
-# Different appsettings json files are used for the dev/staging server and the production/release server
-# these are the application names specified in CodeDeploy
-if [ "$APPLICATION_NAME" == "reach-be-deploy-dev" ]
-then
-    nohup /usr/local/bin/dotnet --environment "Development" HourglassServer.dll &>$LOG_FILE &
-fi
-
-if [ "$APPLICATION_NAME" == "reach-be-deploy-prod" ]
-then
-    nohup /usr/local/bin/dotnet --environment "Production" HourglassServer.dll &>$LOG_FILE &
-fi
-
-
+nohup /usr/local/bin/dotnet HourglassServer.dll &>$LOG_FILE &
